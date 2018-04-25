8f0f5da7b20dc5ae2372815db384c26e11b9748d
Grimont <grimont.c@gmail.com>
2018-04-25 16:10:26 +0200

    fix version

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,4 @@
-import {C8oSettings} from "c8osdkjscore/src/c8o/c8oSettings";
-
 export * from "./c8o/c8o.service";
 export * from "./c8o/c8oUtils.service";
-
-
 export * from "c8osdkjscore";
-//export * from "../node_modules/c8osdkjscore/src/c8o/c8oSettings";
-/*export * from "./c8o/c8oBase";
-export * from "./c8o/c8oCallTask";
-export * from "./c8o/fullsyncpolicy";
-export * from "./c8o/c8oFullSyncChangeListener";
-export * from "./c8o/c8oFullSyncCbl";
-export * from "./c8o/fullsyncpostdocumentparameter";
-export * from "./c8o/c8oFullSync";
-export * from "./c8o/c8oFullSyncTranslator";
-export * from "./c8o/c8oHttpInterface";
-export * from "./c8o/c8oLocalCache";
-export * from "./c8o/c8oLocalCacheResponse";
-export * from "./c8o/c8oLogger";
-export * from "./c8o/c8oLogLevel";
-export * from "./c8o/c8oProgress";
-export * from "./c8o/c8oPromise";
-export * from "./c8o/c8oResponse";
-export * from "./c8o/c8oSettings";
-export * from "./c8o/c8oTranslator";
-export * from "./c8o/c8oUtilsCore";
-export * from "./c8o/fullSyncAttachmentParameter";
-export * from "./c8o/fullSyncDatabase";
-export * from "./c8o/fullSyncDeleteDocumentParameter";
-export * from "./c8o/fullSyncGetDocumentParameter";
-export * from "./c8o/fullSyncGetViewParameter";
-export * from "./c8o/fullSyncreplication";
-export * from "./c8o/fullSyncRequestable";
-export * from "./c8o/fullSyncResponse";
-export * from "./c8o/priority.service";
-
-export * from "./c8o/Exception/c8oCouchBaseLiteException";
-export * from "./c8o/Exception/c8oException";
-export * from "./c8o/Exception/c8oExceptionListener";
-export * from "./c8o/Exception/c8oExceptionMessage";
-export * from "./c8o/Exception/c8oHttpRequestException";
-export * from "./c8o/Exception/c8oRessourceNotFoundException";
-export * from "./c8o/Exception/c8oUnavailableLocalCacheException";*/
 
