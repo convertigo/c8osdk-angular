c9aa62221916ff4dfd8c886cb7c5203462da3d29
Charles Grimont <grimont.c@gmail.com>
2019-03-22 17:28:00 +0100

    optimize

--- projects/c8osdkangular/src/lib/c8oHttpInterface.service.ts
+++ projects/c8osdkangular/src/lib/c8oHttpInterface.service.ts
@@ -6,8 +6,6 @@ export class C8oHttpInterface extends C8oHttpInterfaceCore{
     
     constructor(c8o: C8o) {
         super(c8o);
-        var a = HttpEventType.UploadProgress;
-        console.log(HttpEventType.UploadProgress.toString())
     }
 
     /**
@@ -23,30 +21,20 @@ export class C8oHttpInterface extends C8oHttpInterfaceCore{
      * @param url the url to post
      * @param form the form data to post
      * @param headersObject Headers to use
-     * @param progress C8oprogress object
-     * @param parameters Given parameters
-     * @param c8oResponseListener ResponseListener
-     * @param varNull null var
-     * @param resolve parents header
      */
-    public getuploadRequester(url:string, form: FormData, headersObject:any, progress: C8oProgress, parameters: Object, c8oResponseListener: C8oResponseListener, varNull:any, resolve:any) {
+    public getuploadRequester(url:string, form: FormData, headersObject:any):Observable<any>{//, progress: C8oProgress, parameters: Object, c8oResponseListener: C8oResponseListener, varNull:any, resolve:any) {
         let headers = new HttpHeaders(headersObject);
         const httpRequest = new HttpRequest('POST', url, form, {reportProgress: true, withCredentials: true, headers: headers});
-        this.c8o.httpPublic.request(httpRequest)
-        .subscribe(
-            event=>{
-                if (event.type === HttpEventType.UploadProgress) {
-                    this.handleProgress(event, progress, parameters, c8oResponseListener, varNull);
-                } else if (event instanceof HttpResponse) {
-                    resolve(event);
-                }
-            },
-            error => { 
-                resolve({"error" :(new C8oHttpRequestException(C8oExceptionMessage.runHttpRequest(), error))
-            });
-        });
+        return this.c8o.httpPublic.request(httpRequest);
     }
     
+    /**
+     * test type of request
+     * @param event any
+     */
+    public isHttpResponse(event:any):boolean{
+        return (event instanceof HttpResponse)
+    }
 
     
 
