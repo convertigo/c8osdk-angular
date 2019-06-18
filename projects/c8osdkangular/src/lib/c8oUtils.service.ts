c6b3e8b1903e9588521e139f72d1cc801725bffc
Charles Grimont <grimont.c@gmail.com>
2019-06-18 18:24:30 +0200

    Fix #28 remove old import of old http lib

--- projects/c8osdkangular/src/lib/c8oUtils.service.ts
+++ projects/c8osdkangular/src/lib/c8oUtils.service.ts
@@ -1,13 +1,13 @@
 import {C8oUtilsCore} from "../c8osdk-js-core/src/index";
 //import {C8oUtilsCore} from "c8osdkjscore";
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable()
 export class C8oUtils extends C8oUtilsCore{
 
-    public constructor(private http: Http) {
+    public constructor(private http: HttpClient) {
         super();
         this.http = http;
     }
