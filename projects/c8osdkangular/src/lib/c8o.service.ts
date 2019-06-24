1aa25699f8acb7d8e8e3a97dd175f9d927ad6225
Charles Grimont <grimont.c@gmail.com>
2019-06-24 17:01:03 +0200

    beta21

--- projects/c8osdkangular/src/lib/c8o.service.ts
+++ projects/c8osdkangular/src/lib/c8o.service.ts
@@ -16,7 +16,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "3.0.0-beta20";
+        return "3.0.0-beta21";
     }
     /**
      * Method to bastract http get
