6f1accb90b45afde973cff37fef998fc947f9fa2
Charles Grimont <grimont.c@gmail.com>
2021-03-19 14:34:11 +0100

    ref https://github.com/convertigo/c8osdk-js-core/issues/25 (Sequences with a file upload returns a wrong response)
    - fixed
    - versionning

--- projects/c8osdkangular/src/lib/c8o.service.ts
+++ projects/c8osdkangular/src/lib/c8o.service.ts
@@ -15,7 +15,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "3.0.12";
+        return "3.0.13-beta1";
     }
     /**
      * Method to bastract http get
@@ -31,4 +31,5 @@ export class C8o extends C8oCore {
     public initC8oHttInterface(){
         this.httpInterface = new C8oHttpInterface(this);
     }
+}  }
 }
