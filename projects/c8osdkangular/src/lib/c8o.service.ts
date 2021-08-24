42df37469cbb9c553370d9722ae6333715563c5a
Charles Grimont <grimont.c@gmail.com>
2021-08-24 13:03:01 -0500

    ref #55
    doing

--- projects/c8osdkangular/src/lib/c8o.service.ts
+++ projects/c8osdkangular/src/lib/c8o.service.ts
@@ -15,7 +15,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "3.0.16-beta5";
+        return "3.0.16-beta6";
     }
     /**
      * Method to bastract http get
