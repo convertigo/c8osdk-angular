99c731e219ec8f85e8ddfb16a073c5e996b64d2b
Charles Grimont <grimont.c@gmail.com>
2021-09-09 12:07:59 -0500

    ref https://github.com/convertigo/c8osdk-angular/issues/56
    done

--- projects/c8osdkangular/src/lib/c8o.service.ts
+++ projects/c8osdkangular/src/lib/c8o.service.ts
@@ -15,7 +15,7 @@ export class C8o extends C8oCore {
      * get current sdk version
      */
     public get sdkVersion(): string {
-        return "3.0.16-beta7";
+        return "3.0.16-beta10";
     }
     /**
      * Method to bastract http get
