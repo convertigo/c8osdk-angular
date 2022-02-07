fe37917128f0ac214ca19242e6642c207ede0330
Charles GRIMONT <grimont.c@gmail.com>
2022-02-07 10:20:30 +0100

    remove c8osdk-js-core submodule, and add c8osdk-angular-core submodule

--- projects/c8osdkangular/src/public_api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of c8osdkangular
- */
-export * from "./lib/c8o.service";
-export * from "./lib/c8oUtils.service";
-export * from "./lib/c8oHttpInterface.service";
-export * from "./lib/c8oHttpxsrfInterceptor.service";
-//export * from "c8osdkjscore";
-export * from "./c8osdk-js-core/src/index";
