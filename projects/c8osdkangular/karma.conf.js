eb9f0dde194d426e1815fff03a1f3b06cb1ba23e
Charles GRIMONT <grimont.c@gmail.com>
2022-02-08 14:40:41 +0100

    working on tests

--- projects/c8osdkangular/karma.conf.js
+++ projects/c8osdkangular/karma.conf.js
@@ -14,7 +14,10 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false // disable the random running order
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
